5d2f72219410865da6036fddef16b6e76c0ad321
vikash-tiwary <vikash.tiwary@sts.in>
2021-09-20 18:49:18 +0530

    added updated models.py

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.String(200))
-    author = db.Column(db.String(100))
+    author = db.relationship('Author',backref='post')
 
     def __init__(self,title,author,description):
         self.title=title
@@ -33,9 +33,17 @@ class Author(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
     name = db.Column(db.String(100))
+    address = db.relationship('Address',backref='author')
+    def __init__(self,post_id,name,address):
+        self.post_id=post_id
+        self.name=name
+        self.address=address
 
 class Address(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
     description = db.Column(db.String(500))
+    def __init__(self,author_id,name):
+        self.author_id=author_id
+        self.description=description
     
